effc87987c44dae76d9bc8343ae099600bddc0a1
tomron27 <37380608+tomron27@users.noreply.github.com>
2018-12-29 20:55:05 +0200

    data loading

--- utils.py
+++ utils.py
@@ -1,3 +1,29 @@
+import numpy as np
+import pandas as pd
 
-def test():
-    print('Hello!')
+def estimate_uplift(data, T_label, Y_label):
+    """
+    Estiamte the difference in means between two groups.
+
+    Parameters
+    ----------
+    data: pandas.DataFrame
+        a dataframe of samples.
+
+    Returns
+    -------
+    estimated_uplift: dict[Str: float] containing two items:
+        "estimated_effect" - the difference in mean values of $y$ for treated and untreated samples.
+        "standard_error" - 90% confidence intervals arround "estimated_effect"
+
+
+    """
+    base = data[data[T_label] == 0]
+    variant = data[data[T_label] == 1]
+
+    delta = variant[Y_label].mean() - base[Y_label].mean()
+    delta_err = 1.96 * np.sqrt(
+        variant[Y_label].var() / variant.shape[0] +
+        base[Y_label].var() / base.shape[0])
+
+    return {"estimated_effect": delta, "standard_error": delta_err}
